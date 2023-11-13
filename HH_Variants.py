6f6021c93235fc4c9442355467efb439fe681c38
Jordan Matuszewski <jmatuszewski@sandiego.edu>
2023-11-13 04:26:22 -0500

    Update HH_Variants.py

--- HH_Variants.py
+++ HH_Variants.py
@@ -798,5 +798,4 @@ def update_graph(steps,sequence,param):
 
     return IETsr, value, marks, figure1, figure2, figure3, figure4, figure5, figure6
 
-if __name__ == '__main__':
-    app.run(debug=False)
+app.run_server(debug=False)
